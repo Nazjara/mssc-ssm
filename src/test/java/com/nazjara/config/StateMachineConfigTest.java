5d5f2695af2f9d3f81883146007ae58e3d9b33ea
Nazar Lipkovskyy <nazar.lipkovskyy@gmail.com>
2019-11-24 16:06:52 +0200

    Add StateMachineConfig

---
+++ src/test/java/com/nazjara/config/StateMachineConfigTest.java
@@ -0,0 +1,33 @@
+package com.nazjara.config;
+
+import com.nazjara.model.PaymentEvent;
+import com.nazjara.model.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = factory.getStateMachine();
+
+        stateMachine.start();
+
+        System.out.println(stateMachine.getState().toString());
+
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTH);
+
+        System.out.println(stateMachine.getState().toString());
+
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+
+        System.out.println(stateMachine.getState().toString());
+    }
+}
